ec7765a42ecbcbb27263356fa01e7a168daae8f9
Helen <howareyouhelen@hotmail.com>
2021-10-07 12:15:52 -0700

    change display name

--- Models/Province.cs
+++ Models/Province.cs
@@ -3,7 +3,10 @@ using System.ComponentModel.DataAnnotations;
 
 public class Province {
     [Key]
+    
+    [Display (Name="Province Code")]
     public string ProvinceCode { get; set; }
+    [Display (Name="Province")]
     public string ProvinceName { get; set; }
 
     public List<City> Cities { get; set; }
